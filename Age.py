4a814d19a8a462d1264cda71cfbc750445e4119c
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-08-31 19:44:05 +0530

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,6 @@
+# Write a program that asks the user to enter their name and their age. 
+# Print out a message that greets them and that tells them the year that they will turn 100 years old.
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("Hi"+" "+name+",Year when you will turn 100 is:",100-age+2022)
