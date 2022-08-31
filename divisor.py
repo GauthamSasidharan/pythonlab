cb558ce58839822ee4fd8c46e7fde6d733213c4c
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-08-31 23:42:52 +0530

    Add files via upload

---
+++ divisor.py
@@ -0,0 +1,7 @@
+#Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+
+n=int(input("Enter an integer:"))
+print("The divisors of the number are:")
+for i in range(1,n+1):
+    if(n%i==0):
+        print(i)
