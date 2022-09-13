f5a5f05c145610bbc66571913796cbb87dcb4028
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-09-13 08:50:38 +0530

    Add files via upload

---
+++ 4thelement.py
@@ -0,0 +1,13 @@
+# Write a Python program to get the 4th element and 4th element from last of a tuple.
+
+
+
+
+t = ("M", "a", "s", "h", "u", "p", "s", "t", "a", "c","k")
+print(t)
+
+i = t[3]
+print("4th Element :",i)
+
+j = t[-4]
+print("4th Element From Last:",j)
