42586a63144c7bd993216f450cdb031140c2e98a
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-09-13 22:30:40 +0530

    Add files via upload

--- sublist.py
+++ sublist.py
@@ -1,17 +1,9 @@
 # Write a Python program to generate all sublists of a list.
-given_list = list()
-result_list = list()
-
-size = int(input('Enter the size of the list :'))
-
-print('Enter all elements of the list :')
-
-for i in range(size):
-    given_list.append(int(input('Enter element to add : ')))
-
-for i in range(len(given_list) + 1):
-    for j in range(i + 1, len(given_list) + 1):
-        result_list.append(given_list[i:j])
-
-print(given_list)
-print(result_list)
+l1 = [1, 2, 3]
+def sub_lists (l):
+    lists = [[]]
+    for i in range(len(l) + 1):
+        for j in range(i):
+            lists.append(l[j: i])
+    return lists
+print(sub_lists(l1))
