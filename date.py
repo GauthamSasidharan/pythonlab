dfd2dfd170ed777c7bbd734fb78e0e81771d6334
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-09-17 19:49:11 +0530

    Update date.py

--- date.py
+++ date.py
@@ -1,3 +1,13 @@
+# 1. Write a Python script to display the
+# a) Current date and time
+# b) Current year
+# c) Month of year
+# d) Week number of the year
+# e) Weekday of the week
+# f) Day of year
+# g) Day of the month
+# h) Day of week
+
 import datetime
 print("Current date and time: " , datetime.datetime.now())
 print("Current year: ", datetime.date.today().strftime("%Y"))
