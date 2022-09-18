e11042680dc5b2d16c9bc1b6e130806db5f49893
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-09-18 18:03:05 +0530

    Add files via upload

---
+++ longestword.py
@@ -0,0 +1,10 @@
+# Write a python program to find the longest words.
+f = open("test.txt","r")
+str = f.read()
+words = str.split()
+max_len = len(max(words, key=len))
+for word in words:
+    if len(word)==max_len:
+        longest_word =word
+         
+print(longest_word)
