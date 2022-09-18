2a12d032fcab012dd5d80ec110c7cd95c85629c0
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-09-18 18:24:31 +0530

    Add files via upload

--- frequency.py
+++ frequency.py
@@ -1,10 +1,7 @@
-# Write a Python program to count the number of characters (character frequency) in a string.
+# Write a Python program to count the frequency of words in a file.
+from collections import Counter
+def word_count(fname):
+        with open(fname) as f:
+                return Counter(f.read().split())
 
-str1 = input ("Enter the string: ")
-d = dict()
-for i in str1:
-    if i in d:
-        d[i] = d[i] + 1
-    else:
-        d[i] = 1
-print(d)
+print("Number of words in the file :",word_count("test.txt"))
