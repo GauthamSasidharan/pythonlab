07503b79d6a12463988e360fe39e070c2fd218ef
Gautham Sasidharan <111673238+GauthamSasidharan@users.noreply.github.com>
2022-09-19 09:44:19 +0530

    Update copyfile.py

--- copyfile.py
+++ copyfile.py
@@ -1,4 +1,6 @@
-# open both files
+# Write a Python program to copy the contents of a file to another file .
+
+
 with open('firsttest.txt','r') as firstfile, open('secondtest.txt','a') as secondfile:
  
  # read content from first file
